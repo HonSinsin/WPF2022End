f89aa2b8de300cb3039ad8099593f67ac60b1864
HonSinsin <fx130903@outlook.com>
2022-03-17 22:28:58 +0800

    【增加】P4 数据模板 Demo

---
+++ WpfApp1/WpfApp1/Views/P4DataTemplate.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// P4DataTemplate.xaml 的交互逻辑
+    /// </summary>
+    public partial class P4DataTemplate : Window
+    {
+        public P4DataTemplate()
+        {
+            InitializeComponent();
+
+            // 即将淘汰的数据绑定方法
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    list.Items.Add(new ListBoxItem()
+            //    {
+            //        Content = new TextBlock() { Text = i.ToString() }
+            //    });
+            //    //list.Items.Add(i.ToString());
+            //}
+
+            List<Color> colors = new List<Color>();
+            colors.Add(new Color() { Code = "red", Name = "红色" });
+            colors.Add(new Color() { Code = "blue", Name = "蓝色" });
+            colors.Add(new Color() { Code = "black", Name = "黑色" });
+            colors.Add(new Color() { Code = "green", Name = "绿色" });
+            colors.Add(new Color() { Code = "pink", Name = "粉色" });
+            list.ItemsSource = colors;
+            grid.ItemsSource = colors;
+        }
+    }
+
+    public class Color
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
