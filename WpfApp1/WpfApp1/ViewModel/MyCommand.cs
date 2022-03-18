4687ceb42b6cdf6dd1e6ccbab072ce7b5978d021
HonSinsin <fx130903@outlook.com>
2022-03-18 23:28:29 +0800

    【添加】P7 Command 以及 对应理解文档

---
+++ WpfApp1/WpfApp1/ViewModel/MyCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    public class MyCommand : ICommand
+    {
+        // 创建委托接受创建命令时的回调函数
+        Action ExecuteAction;
+        public event EventHandler CanExecuteChanged;
+        public MyCommand(Action action)
+        {
+            ExecuteAction = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteAction();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/ViewModel/P7CommandViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.ViewModel
+{
+    public class P7CommandViewModel
+    {
+        // UI 上可绑定的命令字段
+        public MyCommand ShowCommand { get; set; }
+
+        public P7CommandViewModel()
+        {
+            // 给命令绑定执行逻辑回调
+            ShowCommand = new MyCommand(Show);
+        }
+
+        public void Show()
+        {
+            Name = "点击了按钮";
+            MessageBox.Show(Name);
+        }
+
+        public string Name { get; set; } = "张三";
+    }
+}
