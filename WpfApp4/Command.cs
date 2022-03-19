71b2d194bbbd1b78fe6d4bfa3642bf09988dc330
HonSinsin <fx130903@outlook.com>
2022-03-19 21:42:08 +0800

    【添加】P9 MVVM Light 包使用方法

---
+++ WpfApp4/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp4
+{
+    public class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public Action action;
+        public Command(Action act)
+        {
+            action = act;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            //throw new NotImplementedException();
+            action();
+        }
+    }
+}
---
+++ WpfApp4/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp4
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            //ShowCommand = new RelayCommand(Show);
+            ShowCommand = new RelayCommand<string>(Show);
+        }
+
+        //public RelayCommand ShowCommand { get; set; }
+        public RelayCommand<string> ShowCommand { get; set; }
+
+        public void Show(string content)
+        {
+            Name = "点击了按钮";
+            Title = "更新抬头";
+            //MessageBox.Show(content);
+
+            Messenger.Default.Send(content, "ShowMBox");
+        }
+
+        private string name = "123";
+        public string Name { get => name; set { name = value; RaisePropertyChanged(); } }
+
+        private string title = "title";
+        public string Title { get => title; set { title = value; RaisePropertyChanged(); } }
+
+    }
+}
