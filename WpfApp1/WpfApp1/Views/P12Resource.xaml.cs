52cc24ab1a909cd863bf6348fc30ac6f0309f582
HonSinsin <fx130903@outlook.com>
2022-03-20 23:40:02 +0800

    【添加】P12 Resource 资源

---
+++ WpfApp1/WpfApp1/Views/P12Resource.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// P12Resource.xaml 的交互逻辑
+    /// </summary>
+    public partial class P12Resource : Window
+    {
+        public P12Resource()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["BorderStyle"] = new SolidColorBrush(Colors.Cyan);
+        }
+    }
+}
