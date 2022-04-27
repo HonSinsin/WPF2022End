a8e5e6d1ebad97e217d4fc81070ddf53476bedcd
HonSinsin <fx130903@outlook.com>
2022-04-27 23:43:49 +0800

    【P25】设计首页

---
+++ WPFTodo/Common/Models/TaskBar.cs
@@ -0,0 +1,66 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTodo.Common.Models
+{
+    /// <summary>
+    /// 任务栏
+    /// </summary>
+    public class TaskBar : BindableBase
+    {
+        private string icon;
+        private string title;
+        private string context;
+        private string color;
+        private string target;
+
+        /// <summary>
+        /// 图标
+        /// </summary>
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Context
+        {
+            get { return context; }
+            set { context = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 背景颜色
+        /// </summary>
+        public string Color
+        {
+            get { return color; }
+            set { color = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 触发目标
+        /// </summary>
+        public string Target
+        {
+            get { return target; }
+            set { target = value; RaisePropertyChanged(); }
+        }
+    }
+}
--- WPFTodo/ViewModels/IndexViewModel.cs
+++ WPFTodo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFTodo.Common.Models;
 
 namespace WPFTodo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel : BindableBase
     {
+        private ObservableCollection<TaskBar> taskBarList;
+
+        public IndexViewModel()
+        {
+            taskBarList = new ObservableCollection<TaskBar>();
+            CreateTaskBars();
+        }
+
+        public ObservableCollection<TaskBar> TaskBarList
+        {
+            get { return taskBarList; }
+            set { taskBarList = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTaskBars()
+        {
+            taskBarList.Add(new TaskBar() { Icon = "WebClock", Title = "汇总", Context = "9", Color = "#FF0CA0FF", Target = "", });
+            taskBarList.Add(new TaskBar() { Icon = "ClockCheckOutLine", Title = "已完成", Context = "8", Color = "#FF1ECA3A", Target = "", });
+            taskBarList.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成比例", Context = "89%", Color = "#FF02C6DC", Target = "", });
+            taskBarList.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录", Context = "4", Color = "#FFFFA000", Target = "", });
+
+        }
     }
 }
